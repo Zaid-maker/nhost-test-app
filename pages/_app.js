be740a1c40306e3e8353b8f20bdefeaed9763ef2
Zaid-maker <pzhafeez@gmail.com>
2022-09-10 00:06:12 +0500

    Init App

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,21 @@
 import '../styles/globals.css';
 import { Toaster } from 'react-hot-toast';
 import { UserProvider } from '../UserProvider';
+import { NhostNextProvider, NhostClient } from '@nhost/nextjs'
+
+const nhost = new NhostClient({
+  subdomain: process.env.NEXT_PUBLIC_NHOST_SUBDOMAIN || '',
+  region: process.env.NEXT_PUBLIC_NHOST_REGION || ''
+});
 
 function MyApp({ Component, pageProps }) {
   return (
-    <UserProvider>
-      <Component {...pageProps} />
-      <Toaster />
-    </UserProvider>
+    <NhostNextProvider nhost={nhost} initial={pageProps.nhostSession}>
+      <UserProvider>
+        <Component {...pageProps} />
+        <Toaster />
+      </UserProvider>
+    </NhostNextProvider>
   );
 }
 
